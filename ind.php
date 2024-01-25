92ff3913da7f7751e815a4dfac1e2f6125ee137b
BKiptoo <bettsonk@gmail.com>
2024-01-25 16:55:53 +0300

    datatypes

---
+++ ind.php
@@ -0,0 +1,14 @@
+<?php
+$num = 12;
+$pi = 3.14;
+$name = "Board infinite";
+$colours = array("red", "green", "blue");
+
+// Using print_r to display the elements of the array
+echo "Colors: ";
+print_r($colours);
+
+// Or, using var_dump
+echo "Colors: ";
+var_dump($colours);
+?>
